84ed763642880d96840ddc376dbaa8b0ba97ca56
rileyt <mthielvoldt@gmail.com>
2020-08-26 20:01:57 -0700

    calculate from entered time

--- popup.js
+++ popup.js
@@ -1,9 +1,18 @@
 let reservationsBtn = document.getElementById('resBtn');
-let message = document.getElementById('message');
+let checkDateBtn = document.getElementById('checkDate');
+let dateInput = document.getElementById('dateInput');
+let message1 = document.getElementById('message1');
+let message2 = document.getElementById('message2');
 let reservations = null
 
-
-reservationsBtn.onclick = function(element) {
+checkDateBtn.onclick = (element) => {
+  console.log(Date.parse(dateInput.value))
+  fromDate = Date.parse(dateInput.value);
+  let relevant = reservations.filter((res) => filterFn(res, fromDate))
+  availableFrom = 24 - relevant.reduce(reduceFn, 0)
+  message2.innerText = "You will have " + availableFrom + "h available at the entered time"
+}
+reservationsBtn.onclick = (element) => {
   console.log("getReservations button clicked")
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -19,28 +28,24 @@ function processReservations(reservationData=null) {
     reservations = reservationData
   }
   console.log(reservations)
-
-  function filterFn(reservation) {
-    cutoffDate = Date.now() - 3600000*24*14
-    reservationDate = Date.parse(reservation.endTime)
-    within14Days = reservationDate > cutoffDate
-    return (reservation.equipment === 'nanolab') && within14Days
-  }
-  
-  relevant = reservations.filter(filterFn)
+  relevant = reservations.filter((res) => filterFn(res, Date.now()))
   console.log(relevant)
-
   // calculate the amount of time available right now. 
   timeAvailable = 24 - relevant.reduce(reduceFn, 0)
+  message1.innerText = "You have " + timeAvailable + " nanolab hours available right now."
+}
 
-  function reduceFn(prevTotal, reservation) {
-    end   = Date.parse(reservation.endTime)
-    begin = Date.parse(reservation.beginTime) 
-    resHours = (end - begin)/1000/3600
-    console.log(resHours)
-    return prevTotal + resHours
-  }
+function filterFn(reservation, fromDate) {
+  cutoffDate = fromDate - 3600000*24*14
+  reservationDate = Date.parse(reservation.endTime)
+  within14Days = reservationDate > cutoffDate
+  return (reservation.equipment === 'nanolab') && within14Days
+}
 
-  message.innerText = "You have " + timeAvailable + " nanolab hours available right now."
-
+function reduceFn(prevTotal, reservation) {
+  end   = Date.parse(reservation.endTime)
+  begin = Date.parse(reservation.beginTime) 
+  resHours = (end - begin)/1000/3600
+  console.log(resHours)
+  return prevTotal + resHours
 }
